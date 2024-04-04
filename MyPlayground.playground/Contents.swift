2ad5143ffa9955ade061dbba42c8f7dba8e74d6f
albina akhmadieva <a_albina_m@icloud.com>
2024-04-04 15:59:56 +0300

    new

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -33,19 +33,92 @@ func isAnagram(_ s: String, _ t: String) -> Bool {
     if s.count != t.count{
         return false
     }
-    var flag_of_finding = false
+    
+    var s_dct = [Character: Int]()
+    var t_dct = [Character: Int]()
+    
     for c in s {
-        flag_of_finding == false
-        for n in t {
-            print(c, n)
-            if c == n {
-                flag_of_finding = true
-                break
-            }
-        }
-        if flag_of_finding == false {
-            return false
+        if s_dct[c] != nil {
+            s_dct[c] = s_dct[c]! + 1
+        } else {
+            s_dct[c] = 1
         }
     }
-    return true
+    for c in t {
+        if t_dct[c] != nil {
+            t_dct[c] = t_dct[c]! + 1
+        } else {
+            t_dct[c] = 1
+        }
+    }
+    return s_dct == t_dct
 }
+
+/*Given an array of strings strs, group the anagrams together.
+ You can return the answer in any order.*/
+
+func groupAnagrams(_ strs: [String]) -> [[String]] {
+    
+    var str_dct = [String: [String]]()
+    var result = [[String]]()
+    
+    for word in strs {
+        var sortedWord: String = String(word.sorted())
+        if str_dct[sortedWord] != nil {
+            str_dct[sortedWord]?.append(word)
+        } else {
+            str_dct[sortedWord] = [word]
+        }
+    }
+    
+    for array in str_dct.values {
+        result.append(array)
+    }
+    
+    return result
+}
+
+
+func topKFrequent(_ nums: [Int], _ k: Int) -> [Int] {
+    var k_copy = k
+    var resultArray = [Int]()
+    var nums_dct = [Int: Int]()
+    
+    for n in nums {
+        if nums_dct[n] != nil {
+            nums_dct[n] = nums_dct[n]! + 1
+        } else {
+            nums_dct[n] = 1
+        }
+    }
+    
+    var swapedDct = [Int: Int]()
+    
+    for (key, value) in nums_dct {
+        swapedDct[value] = key
+    }
+    
+    return resultArray
+}
+
+/*Given an integer array nums, return an array answer such that answer[i]
+ is equal to the product of all the elements of nums except nums[i].*/
+
+func productExceptSelf(_ nums: [Int]) -> [Int] {
+    var numsRes = Array(repeating: 1, count: nums.count)
+
+    for i in 1..<nums.count {
+        numsRes[i] = nums[i - 1] * numsRes[i - 1]
+    }
+    
+    var mul = 1
+    
+    for i in (0..<nums.count).reversed(){
+        numsRes[i] *= mul
+        mul *= nums[i]
+    }
+
+    return numsRes
+}
+
+productExceptSelf([1, 2, 3, 4])
