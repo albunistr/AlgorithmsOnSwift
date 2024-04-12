278f9bebbf3929e15d6263b2925a9d4fbe2ec2b6
albina akhmadieva <a_albina_m@icloud.com>
2024-04-12 18:55:57 +0300

    new binsearch

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -328,5 +328,29 @@ func lengis (_ A: [Int]) -> Int {
     
     return F[A.count][B.count]
 }
+/*There is an integer array nums sorted in ascending order (with distinct values).
+ 
+ Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
 
-lengis([1, 2, 3, 4])
+ Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+ You must write an algorithm with O(log n) runtime complexity.*/
+func searchInRotatedSortedArray(_ nums: [Int], _ target: Int) -> Int {
+    var (left, right) = (0, nums.count - 1)
+
+    while left <= right {
+        if nums[left] == target {
+            return left
+
+        } else if nums[right] == target {
+            return right
+
+        } else {
+            left += 1
+            right -= 1
+        }
+    }
+        
+    return -1
+}
+
